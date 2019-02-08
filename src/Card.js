561e00c16e0aae5447b7c5dae861a0b7f601adfc
emilerik <emil.eriksson41@gmail.com>
2019-02-08 13:43:56 +0100

    Added Scroll function, SearchBox, json fetching of users

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
 	return (
 		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
+			<img alt='robots' src={`https://robohash.org/${id}?size=220x220`} />
 			<div>
-				<h2>{name}</h2>
+				<h3>{name}</h3>
 				<p>{email}</p>
 			</div>
 		</div>
